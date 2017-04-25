db0164305463ced89cab4677546b17b301e53d52
Tatiana Liatambur <T.Goroshina@yandex.ru>
2017-04-25 18:43:07 +0300

    added mexins and start to do mobile first

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,68 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSynk = require('browser-sync'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs');
+    uglify = require('gulp-uglifyjs'),
+    concatCss = require('gulp-concat-css'),
+    uglifyCss = require('gulp-uglifycss'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
 
-gulp.task ('sass', function () {
-    return gulp.src('app/sass/*.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSynk.reload({stream: true}))
+
+gulp.task ('scripts', function () {
+    return gulp.src('app/js/*.js')
+        .pipe(concat('main.script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSynk.reload({stream: true}));
 });
 
-// gulp.task ('scripts', function () {
-//     return gulp.src()
-//
-// });
+gulp.task ('styles', function () {
+    return gulp.src('app/scss/main.scss')
+        .pipe(sass())
+        .pipe(concatCss('main.style.min.css'))
+        .pipe(uglifyCss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSynk.reload({stream: true}));
+});
+
+gulp.task ('html', function () {
+    gulp.src('app/index.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSynk.reload({stream: true}));
+});
+
+gulp.task ('images', function () {
+    gulp.src ('app/img/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/img'))
+        .pipe(browserSynk.reload({stream: true}));
+});
+
+gulp.task ('fonts', function () {
+    gulp.src('app/fonts/*.ttf')
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe(browserSynk.reload({stream: true}));
+});
 
 gulp.task('browser-sync', function () {
     browserSynk({
         server: {
-            baseDir: 'app'
+            baseDir: 'dist'
         }
     })
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function () {
-    gulp.watch('app/sass/**/*.sass', ['sass']);
-    gulp.watch('app/*.html', browserSynk.reload);
-    gulp.watch('app/js/**/*.js', browserSynk.reload);
+gulp.task('build-all', ['scripts','styles', 'images', 'html', 'fonts']);
+
+gulp.task('watch', ['browser-sync', 'build-all'], function () {
+    gulp.watch('app/scss/**/*.scss', ['styles']);
+    gulp.watch('app/*.html', ['html']);
+    gulp.watch('app/js/**/*.js', ['scripts']);
+    gulp.watch('app/img/**/*.+(png|jpg|gif|svg)', ['images']);
+    gulp.watch('app/fonts/**/*.ttf', ['fonts']);
 });
